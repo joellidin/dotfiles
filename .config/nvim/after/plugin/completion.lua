c4b7378ffe008943d5955d52ef669d5c94754bd9
Joel Lidin <joel.lidin@volvocars.com>
2022-04-22 16:25:14 +0200

    Add default mapping for select cmp item

--- .config/nvim/after/plugin/completion.lua
+++ .config/nvim/after/plugin/completion.lua
@@ -66,6 +66,8 @@ local lspkind = require "lspkind"
 
 cmp.setup {
   mapping = {
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-e>"] = cmp.mapping {
