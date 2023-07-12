2ec28a0c5e1467415bb5804ffff2de92c408edbf
Joel Lidin <joel.lidin@volvocars.com>
2023-07-12 11:47:50 +0200

    Remove old lsp configs I don't like
    This removed some features of Pyright that I actually want to use.
    Especially for typing and using Protocols.

--- .config/nvim/lua/joel/lsp.lua
+++ .config/nvim/lua/joel/lsp.lua
@@ -94,7 +94,7 @@ for _, lsp in pairs(servers) do
     settings = {
       python = {
         analysis = {
-          typeCheckingMode = "off",
+          typeCheckingMode = "on",
         },
       },
       ["rust-analyzer"] = {
@@ -129,38 +129,38 @@ local function filter(arr, func)
   end
 end
 
-local function filter_diagnostics(diagnostic)
-  -- Only filter out Pyright stuff for now
-  if diagnostic.source ~= "Pyright" then
-    return true
-  end
-
-  -- Allow kwargs to be unused, sometimes you want many functions to take the
-  -- same arguments but you don't use all the arguments in all the functions,
-  -- so kwargs is used to suck up all the extras
-  if diagnostic.message == '"kwargs" is not accessed' then
-    return false
-  end
-
-  -- Allow variables starting with an underscore
-  if string.match(diagnostic.message, '"_.+" is not accessed') then
-    return false
-  end
-
-  -- Let Ruff warn about this
-  if string.match(diagnostic.message, '".+" is not accessed') then
-    return false
-  end
-
-  return true
-end
-
-local function custom_on_publish_diagnostics(a, params, client_id, c, config)
-  filter(params.diagnostics, filter_diagnostics)
-  vim.lsp.diagnostic.on_publish_diagnostics(a, params, client_id, c, config)
-end
-
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(custom_on_publish_diagnostics, {})
+-- local function filter_diagnostics(diagnostic)
+--   -- Only filter out Pyright stuff for now
+--   if diagnostic.source ~= "Pyright" then
+--     return true
+--   end
+--
+--   -- Allow kwargs to be unused, sometimes you want many functions to take the
+--   -- same arguments but you don't use all the arguments in all the functions,
+--   -- so kwargs is used to suck up all the extras
+--   if diagnostic.message == '"kwargs" is not accessed' then
+--     return false
+--   end
+--
+--   -- Allow variables starting with an underscore
+--   if string.match(diagnostic.message, '"_.+" is not accessed') then
+--     return false
+--   end
+--
+--   -- Let Ruff warn about this
+--   if string.match(diagnostic.message, '".+" is not accessed') then
+--     return false
+--   end
+--
+--   return true
+-- end
+--
+-- local function custom_on_publish_diagnostics(a, params, client_id, c, config)
+--   filter(params.diagnostics, filter_diagnostics)
+--   vim.lsp.diagnostic.on_publish_diagnostics(a, params, client_id, c, config)
+-- end
+--
+-- vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(custom_on_publish_diagnostics, {})
 
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
   border = "double",
