97e6029efd95a81d98dd5b19b10d782fb583696c
Joel Lidin <joel.lidin@volvocars.com>
2024-11-15 13:27:53 +0100

    Update syntax highlighting in commit messages

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -83,6 +83,8 @@ local _ = require("nvim-treesitter.configs").setup {
     "yaml",
   },
 
+  sync_install = false,
+  auto_install = true,
   highlight = {
     enable = true,
     use_languagetree = false,
